e8da749fe86a223fa46b9b9aebff5da91bcd92f0
LEE MINJI <m04j00@gmail.com>
2021-10-30 20:52:57 +0900

    사용 방법 수정

--- PVM_Program/info.cpp
+++ PVM_Program/info.cpp
@@ -1,9 +1,8 @@
 #include "main.h"
-
+   
 void infoScreen() {
 	system("cls");
 	cout << endl << endl << endl << endl;
-	cout << endl << endl;
 	cout << "                                 조작 방법                                 " << endl << endl;
 	cout << "                           이동 : 화살표 ↑ ↓ ← →                          " << endl << endl;
 	cout << "                           선택 : 엔터                           " << endl << endl << endl;
